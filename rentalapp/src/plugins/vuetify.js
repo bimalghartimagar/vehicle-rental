b7e591c8344e0f7577f537ebb339f9b07f045d22
Bimal Gharti Magar <bimal.gharti.magar@gmail.com>
2020-05-25 01:02:49 +0545

    Added colors to theme
    Added google material icons

--- rentalapp/src/plugins/vuetify.js
+++ rentalapp/src/plugins/vuetify.js
@@ -3,4 +3,18 @@ import Vuetify from "vuetify/lib";
 
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+  theme: {
+    themes: {
+      light:  {
+        primary: '#607d8b',
+        secondary: '#ff9800',
+        accent: '#9c27b0',
+        error: '#f44336',
+        warning: '#ffc107',
+        info: '#2196f3',
+        success: '#4caf50'
+        }
+    }
+  },
+});
--- rentalapp/src/views/Login.vue
+++ rentalapp/src/views/Login.vue
@@ -1,5 +1,35 @@
 <template>
-  <div class="about">
-    <h1>This is login page</h1>
-  </div>
+  <v-container class="fill-height" fluid>
+    <v-row align="center" justify="center">
+      <v-col cols="12" sm="8" md="4">
+        <v-card class="elevation-12">
+          <v-toolbar color="primary" dark flat>
+            <v-toolbar-title>Login</v-toolbar-title>
+          </v-toolbar>
+          <v-card-text>
+            <v-form>
+              <v-text-field
+                label="Login"
+                name="login"
+                prepend-icon="person"
+                type="text"
+              ></v-text-field>
+
+              <v-text-field
+                id="password"
+                label="Password"
+                name="password"
+                prepend-icon="lock"
+                type="password"
+              ></v-text-field>
+            </v-form>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="primary">Login</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
