e679d33d74822ba2cf7aa49b7cf6a385fad69471
Bimal Gharti Magar <bimal.gharti.magar@gmail.com>
2020-06-18 18:40:01 +0545

    added condition for building OPTIONS response
    removed logs

--- rentalapi/app.py
+++ rentalapi/app.py
@@ -32,14 +32,14 @@ def create_app():
 
     @app.after_request
     def after_request(response):
-        app.logger.debug("I am here")
-        app.logger.debug(response.headers)
-        app.logger.debug(dir(response))
         # response.headers.add('Access-Control-Allow-Origin', '*')
         # response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
         # response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
         # response.headers.add('Access-Control-Allow-Credentials', 'true')
-        return app.response_class(response=response.data, status=200)
+        if request.method == "OPTIONS":
+            return app.response_class(response=response.data, status=200)
+        else:
+            return response
 
     if app.config['ENV'] == 'development':
         from .dummy_data import insert_dummy_data
