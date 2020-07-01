a94c4620cb336b39d29d3c163140783e1f127411
Bimal Gharti Magar <bimal.gharti.magar@gmail.com>
2020-07-01 17:39:19 +0545

    Login feature and store token to localstorage

--- rentalapp/src/api/rentalApi.js
+++ rentalapp/src/api/rentalApi.js
@@ -1,4 +1,21 @@
 import axios from "axios";
+import localstorageservice from "../utils/localstorage";
+const localStorageService = localstorageservice.getService();
+
+axios.interceptors.request.use(
+  config => {
+    const token = localStorageService.getAccessToken();
+
+    if (token) {
+      config.headers["Authorization"] = "Bearer " + token;
+    }
+    config.headers["Content-Type"] = "application/json";
+    return config;
+  },
+  error => {
+    Promise.reject(error);
+  }
+);
 
 axios.defaults.baseURL = process.env.VUE_APP_BASE_URL;
 
--- rentalapp/src/views/Login.vue
+++ rentalapp/src/views/Login.vue
@@ -2,6 +2,7 @@
   <v-container class="fill-height" fluid>
     <v-row align="center" justify="center">
       <v-col cols="12" sm="8" md="4">
+        <h1 class="text-center">Login to Vehicle Rental</h1>
         <v-card class="elevation-12">
           <v-toolbar color="primary" dark flat>
             <v-toolbar-title>Login</v-toolbar-title>
@@ -13,6 +14,7 @@
                 name="login"
                 prepend-icon="person"
                 type="text"
+                v-model="username"
               ></v-text-field>
 
               <v-text-field
@@ -21,15 +23,55 @@
                 name="password"
                 prepend-icon="lock"
                 type="password"
+                v-model="password"
               ></v-text-field>
             </v-form>
+            <span>
+              {{ errorMsg }}
+            </span>
           </v-card-text>
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn color="primary">Login</v-btn>
+            <v-btn color="primary" @click.prevent="login">Login</v-btn>
           </v-card-actions>
         </v-card>
       </v-col>
     </v-row>
   </v-container>
 </template>
+
+<script>
+import rentalApi from "../api/rentalApi";
+import localstorage from "../utils/localstorage.js";
+
+const localStorageService = localstorage.getService();
+
+export default {
+  data() {
+    return {
+      username: "",
+      password: "",
+      errorMsg: ""
+    };
+  },
+
+  methods: {
+    login: function() {
+      rentalApi
+        .post("auth/login/", {
+          username: this.username,
+          password: this.password
+        })
+        .then(response => {
+          localStorageService.setToken(response.data);
+          this.$router.push("/");
+        })
+        .catch(error => {
+          if (error.response.status === 401) {
+            this.errorMsg = error.response.data.msg;
+          }
+        });
+    }
+  }
+};
+</script>
