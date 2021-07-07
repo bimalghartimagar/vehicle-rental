8d12bd3589f783e6f3c128605c43aa44974a677e
Bimal Gharti Magar <bimal.gharti.magar@gmail.com>
2021-07-08 00:15:13 +0545

    api: updated search api to filter vehicles
    * Joined vehicles with rental to filter out booked vehicles
    base on pickup and dropoff dates
    * Passed query params value to filter query

--- rentalapi/resources/search.py
+++ rentalapi/resources/search.py
@@ -1,6 +1,7 @@
-from flask import jsonify, current_app
+from flask import jsonify, request
+
 from rentalapi.schema import SearchSchema
-from rentalapi.dao.models import Vehicles
+from rentalapi.dao.models import Vehicles, Rentals
 
 vehicles_schema = SearchSchema(many=True)
 
@@ -11,9 +12,17 @@ def init_search(api_bp):
   
   @api.route('/search/vehicles')
   def search():
-    current_app.logger.debug('Searching')
-    vehicles = Vehicles.query.all()
-    current_app.logger.debug('Fetched')
-    results = jsonify(vehicles_schema.dump(vehicles))
-    # return vehicles_schema.dump(vehicles)
-    return results
+    pickup = request.args.get('pickup')
+    dropoff = request.args.get('dropoff')
+    pickup_date = request.args.get('pickupDate')
+    dropoff_date = request.args.get('dropoffDate')
+    age = request.args.get('age')
+    
+    query = Vehicles.query.join(Rentals, Vehicles.id==Rentals.vehicle_id, isouter=True)
+    query = query.filter(
+      (Rentals.pickup_date == None) | 
+      (~Rentals.pickup_date.between(pickup_date, dropoff_date)) &
+      (~Rentals.dropoff_date.between(pickup_date, dropoff_date))
+    )
+    vehicles = query.all()
+    return jsonify(vehicles_schema.dump(vehicles))
