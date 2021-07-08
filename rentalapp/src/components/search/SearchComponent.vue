de1be00065fbd572d16e35dee5d2827891fa42b0
Bimal Gharti Magar <bimal.gharti.magar@gmail.com>
2021-07-08 07:17:24 +0545

    frontend: updated search result item with details

--- rentalapp/src/components/search/SearchComponent.vue
+++ rentalapp/src/components/search/SearchComponent.vue
@@ -1,11 +1,16 @@
 <template>
-  <div class="d-flex flex-wrap justify-space-around w-4">
-    <SearchItem
-      v-for="item in data"
-      :key="item.id"
-      :item="item"
-      class="align-content-space-around mb-8"
-    />
+  <div>
+  <div class="text-center text-h5">
+    {{data.length}} results found
+    </div>
+    <div class="d-flex flex-wrap justify-space-around w-4">
+      <SearchItem
+        v-for="item in data"
+        :key="item.id"
+        :item="item"
+        class="align-content-space-around mb-8"
+      />
+    </div>
   </div>
 </template>
 
@@ -13,18 +18,18 @@
 import SearchItem from "@/components/search/SearchItem";
 export default {
   components: {
-    SearchItem
+    SearchItem,
   },
 
   props: {
     data: {
       type: Array,
-      required: true
+      required: true,
     },
     queryParams: {
       type: Object,
-      required: true
-    }
-  }
+      required: true,
+    },
+  },
 };
 </script>
--- rentalapp/src/components/search/SearchItem.vue
+++ rentalapp/src/components/search/SearchItem.vue
@@ -1,19 +1,40 @@
 <template>
-  <v-card class="mx-auto" max-width="344" outlined>
+  <v-card class="mx-auto" max-width="600" outlined>
     <v-list-item three-line>
       <v-list-item-content>
-        <div class="text-overline mb-4">{{ item.name }}</div>
-        <v-list-item-title class="text-h5 mb-1"> Headline 5 </v-list-item-title>
+        <div class="text-h5 mb-4 font-weight-bold">{{ item.name }}</div>
+        <v-list-item-title class="text-h6 mb-1">
+          {{ item.vendor }}
+        </v-list-item-title>
         <v-list-item-subtitle
-          >Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle
+          ><v-icon color="blue darken-2"> mdi-seat </v-icon
+          >{{ item.seats }} Seats</v-list-item-subtitle
+        >
+        <v-list-item-subtitle
+          ><v-icon color="blue darken-2"> mdi-bag-checked </v-icon
+          >{{ item.seats }} Luggages</v-list-item-subtitle
+        >
+        <v-list-item-subtitle
+          ><v-icon color="blue darken-2"> mdi-car-shift-pattern </v-icon>Rs.{{
+            item.rate
+          }}/day</v-list-item-subtitle
+        >
+        <v-list-item-subtitle
+          ><v-icon color="blue darken-2"> mdi-gas-station </v-icon
+          >{{ item.seats }} km/l</v-list-item-subtitle
+        >
+        <v-list-item-subtitle
+          ><v-icon color="blue darken-2"> mdi-cash-100 </v-icon>Rs.{{
+            item.rate
+          }}/day</v-list-item-subtitle
         >
       </v-list-item-content>
 
-      <v-list-item-avatar tile size="80" color="grey"></v-list-item-avatar>
+      <v-list-item-avatar tile size="200" color="grey"></v-list-item-avatar>
     </v-list-item>
 
     <v-card-actions>
-      <v-btn outlined text>Details </v-btn>
+      <v-btn :to="`vehicle/${item.id}`" color="secondary" light>Details</v-btn>
     </v-card-actions>
   </v-card>
 </template>
@@ -23,8 +44,8 @@ export default {
   props: {
     item: {
       type: Object,
-      required: true
-    }
-  }
+      required: true,
+    },
+  },
 };
 </script>
